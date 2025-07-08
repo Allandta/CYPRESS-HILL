334e01c194b2d5b20754fcb5512212d6bcb3a54d
Don <dlanod33@gmail.com>
2025-07-08 11:35:22 +0100

    Added Axe and image snapshot

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require("cypress");
+const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Existing tasks...
+      on('task', {
+        log(message) {
+          console.log(message);
+          return null;
+        },
+      });
+      // Add this line:
+      addMatchImageSnapshotPlugin(on, config);
+      return config;
     },
   },
 });
+
